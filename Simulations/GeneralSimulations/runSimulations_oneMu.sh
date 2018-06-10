a8620247dcd7ef00d598eed99edda4aceb8c74fa
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-06-10 15:51:51 +0200

    Added functionality to test the influence of the horizontal dependency

---
+++ Simulations/GeneralSimulations/runSimulations_oneMu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -V
+#$ -S /bin/bash
+#$ -cwd
+#$ -m as
+#$ -M m.m.nieboer@umcutrecht.nl
+#$ -l h_vmem=12G
+#$ -l h_rt=2:00:00
+#$ -e noise0.03Mu90
+#$ -o noise0.03Mu90
+
+#Run a simulation with horizontal dependency removed permutation. We do this 100 times for 1 mu and 1 noise level. 
+
+mu=11 #select 11% because this equals a mu of 10%, as the task ID starts from 0 (so the tumor fraction is 90%)
+
+uuid=$(uuidgen)
+
+python runSimulation.py "$mu" "$uuid"
