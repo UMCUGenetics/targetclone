01d95657c6b73b6d01b52a4ba0ae7a196f53d681
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-06-13 08:51:10 +0200

    Added a setting to turn of SNVs in the tree inference

--- Simulations/GeneralSimulations/visualizeHorizontalDependencyInfluence.py
+++ Simulations/GeneralSimulations/visualizeHorizontalDependencyInfluence.py
@@ -10,7 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #1. Define the folders containing the data that we want to visualize
-snpNums = ['snps_100_noSNVsReRun', 'snps_500_noSNVs', 'snps_1000_noSNVs']
+snpNums = ['snps_100_noSNVsRerun', 'snps_500_noSNVs', 'snps_1000_noSNVs']
 motherFolder = sys.argv[1]
 
 #2. Read the errors from these folders for the normal case and corresponding shuffling
