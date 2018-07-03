a80f29b6b8c9305f08cf7aef022ae3f6833966b7
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-03 21:18:27 +0200

    Simulation settins change

--- Simulations/GeneralSimulations/simulationSettings.py
+++ Simulations/GeneralSimulations/simulationSettings.py
@@ -1,6 +1,6 @@
 files = dict(
 
-	outputDir = 'Results/mixedSubclones_31_40_4/',
+	outputDir = 'Results/mixedSubclones_41_50_1/',
 	segmentationFile = '../../TargetClone/InternalData/pq_segmentation.txt',
 	simulationProbabilityFile = "../../TargetClone/InternalData/lossGainProbabilityFile.txt",
 	targetCloneInstance = '../../TargetClone/InternalData/targetClone.pkl'
@@ -13,9 +13,9 @@ general = dict(
 	kmax = 6,
 	cellCycles = 4,
 	noiseLevel = 0.02, #was 0.03
-	numberOfMixedClones = 4, #This is the number of subclones that we mix in on top of the major clone! The name of this setting needs to be better. 
-	maximumMinorCloneFrequency = 40, #This value should change between simulations to see the effects
-	minimumMinorCloneFrequency = 31, #To test between 0 and 10, we set this value to 1. A value of 0 makes no sense for this test.
+	numberOfMixedClones = 1, #This is the number of subclones that we mix in on top of the major clone! The name of this setting needs to be better. 
+	maximumMinorCloneFrequency = 50, #This value should change between simulations to see the effects
+	minimumMinorCloneFrequency = 41, #To test between 0 and 10, we set this value to 1. A value of 0 makes no sense for this test.
 	minimumSNVFrequency = 0.1, #We filter out every SNV below this frequency, as we assume that it comes from the minor clone and is thus contaminating. (Find optimal value for this frequency). This only works for the scripts that use the realVAF!
 	randomTree = False
 )
