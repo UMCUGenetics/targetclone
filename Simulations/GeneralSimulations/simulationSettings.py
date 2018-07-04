e38668606e2b3bb740ab7f4c8d8e88c3470bb30c
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-04 14:55:50 +0200

    Simulation settings change

--- Simulations/GeneralSimulations/simulationSettings.py
+++ Simulations/GeneralSimulations/simulationSettings.py
@@ -1,6 +1,6 @@
 files = dict(
 
-	outputDir = 'Results/snps_10000/',
+	outputDir = 'Results/snps_50000/',
 	segmentationFile = '../../TargetClone/InternalData/pq_segmentation.txt',
 	simulationProbabilityFile = "../../TargetClone/InternalData/lossGainProbabilityFile.txt",
 	targetCloneInstance = '../../TargetClone/InternalData/targetClone.pkl'
@@ -8,7 +8,7 @@ files = dict(
 
 general = dict(
 	numberOfSNVs = 50,
-	numberOfSNPs = 10000,
+	numberOfSNPs = 50000,
 	kmin = 1,
 	kmax = 6,
 	cellCycles = 4,
@@ -22,7 +22,7 @@ general = dict(
 runType = dict(
 	randomMeasurements = False, #Do we want random LAF and random SNVs to be assigned to each sample? 
 	horizontalShuffle = False, #Do we shuffle the LAF measurements randomly within a sample? (To test the influence of the horizontal dependency)
-	segmentation = False, #do we wish to run with segmentation on the SNP measurements? 
+	segmentation = True, #do we wish to run with segmentation on the SNP measurements? 
 	mixedSamples = False,
 	randomTree = False
 )
