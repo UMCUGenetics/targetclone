b195caa0d5456d23823fa78b07c5f0fe775fe97a
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-04 16:18:00 +0200

    Simulation settings change

--- Simulations/GeneralSimulations/simulationSettings.py
+++ Simulations/GeneralSimulations/simulationSettings.py
@@ -1,6 +1,6 @@
 files = dict(
 
-	outputDir = 'Results/snps_100/',
+	outputDir = 'Results/generic_noise0.005/',
 	segmentationFile = '../../TargetClone/InternalData/pq_segmentation.txt',
 	simulationProbabilityFile = "../../TargetClone/InternalData/lossGainProbabilityFile.txt",
 	targetCloneInstance = '../../TargetClone/InternalData/targetClone.pkl'
@@ -8,11 +8,11 @@ files = dict(
 
 general = dict(
 	numberOfSNVs = 50,
-	numberOfSNPs = 100,
+	numberOfSNPs = 500,
 	kmin = 1,
 	kmax = 6,
 	cellCycles = 4,
-	noiseLevel = 0.02, #was 0.03
+	noiseLevel = 0.005, #was 0.03
 	numberOfMixedClones = 4, #This is the number of subclones that we mix in on top of the major clone! The name of this setting needs to be better. 
 	maximumMinorCloneFrequency = 50, #This value should change between simulations to see the effects
 	minimumMinorCloneFrequency = 41, #To test between 0 and 10, we set this value to 1. A value of 0 makes no sense for this test.
