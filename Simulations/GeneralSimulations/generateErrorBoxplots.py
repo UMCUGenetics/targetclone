bcdc89099661f830e9cc85799ee16a19e9b1623d
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-05 11:25:05 +0200

    Updating script to make figures

--- Simulations/GeneralSimulations/generateErrorBoxplots.py
+++ Simulations/GeneralSimulations/generateErrorBoxplots.py
@@ -970,9 +970,9 @@ def readSimulationData(simulationFolderLocation, noiseLevels):
 					
 					#are the indices that we need in here?
 					
-					variantIndices = samples[0].somaticVariantsInd
-					chromosomes = samples[0].measurements.chromosomes
-					positions = samples[0].measurements.starts
+					variantIndices = simulationData.samples[0].somaticVariantsInd
+					chromosomes = simulationData.samples[0].measurements.chromosomes
+					positions = simulationData.samples[0].measurements.starts
 						
 					
 					
