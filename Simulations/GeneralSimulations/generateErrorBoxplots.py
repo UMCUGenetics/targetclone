bad743d432597f9b60a939f5a790f772392cf1aa
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-26 13:42:24 +0200

    Error plotting update changed to mean and fraction of total pairwise samples for the ancestry swap error

--- Simulations/GeneralSimulations/generateErrorBoxplots.py
+++ Simulations/GeneralSimulations/generateErrorBoxplots.py
@@ -15,7 +15,8 @@ from alleles import Alleles
 from mu import Mu
 from math import sqrt
 from copy import deepcopy
-from scipy import stats
+import scipy as sp
+import scipy.stats
 
 import pickle as pkl
 
