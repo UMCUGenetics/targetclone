e8959b588a42911daac0279793f35020b82c2feb
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-26 21:19:30 +0200

    Parameter changes for making figures

--- Simulations/GeneralSimulations/generateErrorBoxplots.py
+++ Simulations/GeneralSimulations/generateErrorBoxplots.py
@@ -981,11 +981,11 @@ def plotAmbiguityScores(noiseLevels, ambiguities, ambiguityScores, ambiguityStds
 	correctedBelowStd = []
 	for std in range(0, len(ambiguityStds[1])):
 		
-		correctedBelowStd.append(errors[std] - ambiguityStds[1][std])
+		correctedBelowStd.append(ambiguityScores[std] - ambiguityStds[1][std])
 	correctedAboveStd = []
 	for std in range(0, len(ambiguityStds[0])):
 		
-		correctedAboveStd.append(ambiguityStds[0][std] - errors[std])
+		correctedAboveStd.append(ambiguityStds[0][std] - ambiguityScores[std])
 	
 	
 	colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
