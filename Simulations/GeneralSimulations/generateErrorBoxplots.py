66dd2d4fe709f9bfb5b55e603448a198eefac175
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-07-26 21:23:02 +0200

    Parameter changes for making figures

--- Simulations/GeneralSimulations/generateErrorBoxplots.py
+++ Simulations/GeneralSimulations/generateErrorBoxplots.py
@@ -1020,29 +1020,29 @@ def plotAmbiguityScores(noiseLevels, ambiguities, ambiguityScores, ambiguityStds
 
 #loop through the noise levels
 
-noiseLevels = [0, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.04, 0.06, 0.08, 0.1]
-#noiseLevels = [0, 0.005]
-ambiguities = []
-ambiguityScores = []
-
-groupedAmbiguityScores = dict()
-
-for noiseLevel in noiseLevels:
-	
-	currentSimulationFolder = simulationFolder + str(noiseLevel) + '/'
-	print "noise level: ", noiseLevel
-	[averageAmbiguities, averageAmbiguityScore, allAmbiguityScores] = computeCorrectAmbiguityScore(LAFAndCombinations, currentSimulationFolder)
-	ambiguities.append(averageAmbiguities)
-	ambiguityScores.append(averageAmbiguityScore)
-	
-	groupedAmbiguityScores[noiseLevel] = allAmbiguityScores
-	
-#commpute the standard deviations
-ambiguityStds = obtainStandardDeviations(groupedAmbiguityScores, ambiguityScores)
-
-plotAmbiguityScores(noiseLevels, ambiguities, ambiguityScores, ambiguityStds)
-
-exit()
+# noiseLevels = [0, 0.005, 0.01, 0.015, 0.02, 0.025, 0.03, 0.04, 0.06, 0.08, 0.1]
+# #noiseLevels = [0, 0.005]
+# ambiguities = []
+# ambiguityScores = []
+# 
+# groupedAmbiguityScores = dict()
+# 
+# for noiseLevel in noiseLevels:
+# 	
+# 	currentSimulationFolder = simulationFolder + str(noiseLevel) + '/'
+# 	print "noise level: ", noiseLevel
+# 	[averageAmbiguities, averageAmbiguityScore, allAmbiguityScores] = computeCorrectAmbiguityScore(LAFAndCombinations, currentSimulationFolder)
+# 	ambiguities.append(averageAmbiguities)
+# 	ambiguityScores.append(averageAmbiguityScore)
+# 	
+# 	groupedAmbiguityScores[noiseLevel] = allAmbiguityScores
+# 	
+# #commpute the standard deviations
+# ambiguityStds = obtainStandardDeviations(groupedAmbiguityScores, ambiguityScores)
+# 
+# plotAmbiguityScores(noiseLevels, ambiguities, ambiguityScores, ambiguityStds)
+# 
+# exit()
 
 #Plot the tree reconstruction errors when we use different measures. C vs. A, Euclidean, SNVs
 #It is not fair to compare to the iterative scores, so we re-compute the score based on A using only one iteration. Otherwise the entire simulations will need to be re-run. 
@@ -1243,39 +1243,39 @@ def plotTreeErrorsDifferentMetrics(noiseLevels, averagedATreeErrors, averagedCTr
 	
 
 #Figure S7
-# 
-# print "parsing simulation data"
-# #Obtain all errors
-# simulationFolder = 'Results/generic_noise'
-# noiseLevels = [0.005, 0.01, 0.015, 0.02, 0.025, 0.03]
-# [allATreeErrors, allCTreeErrors, allSnvTreeErrors, allEuclideanTreeErrors, allMethodTreeErrors] = readSimulationData(simulationFolder, noiseLevels)
-# #Compute the average errors and standard deviations
-# print "computing averages: "
-# averagedATreeErrors = averageData(allATreeErrors, 'T')
-# averagedCTreeErrors = averageData(allCTreeErrors, 'T')
-# averagedSnvTreeErrors = averageData(allSnvTreeErrors, 'T')
-# averagedEuclideanTreeErrors = averageData(allEuclideanTreeErrors, 'T')
-# averagedMethodTreeErrors = averageData(allMethodTreeErrors, 'T')
-# print "computing standard deviations: "
-# stdATreeErrors = obtainStandardDeviations(allATreeErrors, averagedATreeErrors)
-# print "c:"
-# stdCTreeErrors = obtainStandardDeviations(allCTreeErrors, averagedCTreeErrors)
-# print "snv:"
-# stdSnvTreeErrors = obtainStandardDeviations(allSnvTreeErrors, averagedSnvTreeErrors)
-# print "euclidean:"
-# stdEuclideanTreeErrors = obtainStandardDeviations(allEuclideanTreeErrors, averagedEuclideanTreeErrors)
-# print "tree:"
-# stdMethodTreeErrors = obtainStandardDeviations(allMethodTreeErrors, averagedMethodTreeErrors)
-# 
-# allATreeErrors = None
-# 
-# 
-# print "plotting: "
-# #Make a plot of the tree errors
-# plotTreeErrorsDifferentMetrics(noiseLevels, averagedATreeErrors, averagedCTreeErrors, averagedSnvTreeErrors, averagedEuclideanTreeErrors, averagedMethodTreeErrors, stdATreeErrors, stdCTreeErrors, stdSnvTreeErrors,
-# 							   stdEuclideanTreeErrors, stdMethodTreeErrors)
-# 
-# exit()
+
+print "parsing simulation data"
+#Obtain all errors
+simulationFolder = 'Results/generic_noise'
+noiseLevels = [0.005, 0.01, 0.015, 0.02, 0.025, 0.03]
+[allATreeErrors, allCTreeErrors, allSnvTreeErrors, allEuclideanTreeErrors, allMethodTreeErrors] = readSimulationData(simulationFolder, noiseLevels)
+#Compute the average errors and standard deviations
+print "computing averages: "
+averagedATreeErrors = averageData(allATreeErrors, 'T')
+averagedCTreeErrors = averageData(allCTreeErrors, 'T')
+averagedSnvTreeErrors = averageData(allSnvTreeErrors, 'T')
+averagedEuclideanTreeErrors = averageData(allEuclideanTreeErrors, 'T')
+averagedMethodTreeErrors = averageData(allMethodTreeErrors, 'T')
+print "computing standard deviations: "
+stdATreeErrors = obtainStandardDeviations(allATreeErrors, averagedATreeErrors)
+print "c:"
+stdCTreeErrors = obtainStandardDeviations(allCTreeErrors, averagedCTreeErrors)
+print "snv:"
+stdSnvTreeErrors = obtainStandardDeviations(allSnvTreeErrors, averagedSnvTreeErrors)
+print "euclidean:"
+stdEuclideanTreeErrors = obtainStandardDeviations(allEuclideanTreeErrors, averagedEuclideanTreeErrors)
+print "tree:"
+stdMethodTreeErrors = obtainStandardDeviations(allMethodTreeErrors, averagedMethodTreeErrors)
+
+allATreeErrors = None
+
+
+print "plotting: "
+#Make a plot of the tree errors
+plotTreeErrorsDifferentMetrics(noiseLevels, averagedATreeErrors, averagedCTreeErrors, averagedSnvTreeErrors, averagedEuclideanTreeErrors, averagedMethodTreeErrors, stdATreeErrors, stdCTreeErrors, stdSnvTreeErrors,
+							   stdEuclideanTreeErrors, stdMethodTreeErrors)
+
+exit()
 
 
 
