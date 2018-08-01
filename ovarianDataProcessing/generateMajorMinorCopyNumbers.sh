67951b96d2925c20721cb402b6eda3f6bb916e26
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-08-01 12:23:03 +0200

    Added major/minor cn calling script for ovarain data

--- ovarianDataProcessing/generateMajorMinorCopyNumbers.sh
+++ ovarianDataProcessing/generateMajorMinorCopyNumbers.sh
@@ -9,8 +9,11 @@ for d in "$folder"/*/ ; do
 	#1. First get the cns file
 	cnsFile=`ls "$d"/cnvKit/*.cns`
 	
+	echo "cns file: $cnsFile"
+	
 	#2. Also read the right vcf file
-	vcfFile=`ls "$d"*._filtered.vcf`
+	vcfFile=`ls "$d"*_filtered.vcf`
+	echo "vcf file: $vcfFile"
 	
 	#3. Get the purity from the theta output
 	thetaOutFile=`ls "$d"/cnvKit/*.n2.results`
@@ -31,9 +34,6 @@ for d in "$folder"/*/ ; do
 	done < "$thetaOutFile"
 	
 	#4. Then call cnvkit
-	echo "$cnsFile"
-	
-	echo "$vcfFile"
 	
 	cnvkit.py call "$cnsFile" -v "$vcfFile" --purity "$tumorMu" -x female -m clonal -o "$cnsFile".call
 	
