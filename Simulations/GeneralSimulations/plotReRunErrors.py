d3b7071445024f09718b0c90ea5d8b79b767ee15
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-08-07 10:47:25 +0200

    Adding code to show the rerun differences

--- Simulations/GeneralSimulations/plotReRunErrors.py
+++ Simulations/GeneralSimulations/plotReRunErrors.py
@@ -7,6 +7,7 @@
 
 import sys
 from glob import glob
+from pylab import plot, figure
 
 mainDir = sys.argv[1] #where to read the reruns from
 
@@ -27,23 +28,30 @@ for differenceFile in differenceFiles:
 		for line in inF:
 			line = line.strip()
 			if lineCount == 0:
-				cDifferences.append(line)
+				cDifferences.append(float(line))
 			if lineCount == 1:
-				aDifferences.append(line)
+				aDifferences.append(float(line))
 			if lineCount == 2:
-				muDifferences.append(line)
+				muDifferences.append(float(line))
 			if lineCount == 3:
-				treeDifferences.append(line)
+				treeDifferences.append(float(line))
 				
 			lineCount += 1
 			
 	
 
 #Make a boxplot of the results
-print cDifferences
-print treeDifferences
-	
-		
+
+fig = figure()
+hold(True)
+
+#Plot the boxplots
+bp = boxplot(cDifferences, positions = [1], widths = 0.6)
+bp = boxplot(aDifferences, positions = [2], widths = 0.6)
+bp = boxplot(muDifferences, positions = [3], widths = 0.6)
+bp = boxplot(treeDifferences, positions = [4], widths = 0.6)
+
+show()		
 			
 			
 		
